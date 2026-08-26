68c2e08e9936517e667d2e60220db6e39b7fadc5
Your Name <sbavana045@gmail.com>
2026-08-26 15:59:17 +0530

    pattern

--- patterns/pattern9.java
+++ patterns/pattern9.java
@@ -1,10 +1,23 @@
+/* 
+to print:
+Enter num: 5
+        * 
+      * * * 
+    * * * * * 
+  * * * * * * * 
+* * * * * * * * * 
+  * * * * * * * 
+    * * * * * 
+      * * * 
+        * 
+*/
 import java.util.Scanner;
 class pattern9{
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         System.out.print("Enter num: ");
         int n = input.nextInt();
-        for(int i = 1; i <= n ; i++){
+        for(int i = 1; i <= n-1; i++){
             for(int j = 1; j <= n -i; j++){
                 System.out.print("  ");
             }
